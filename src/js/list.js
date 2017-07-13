6075cff021e1deb707cab4a0ad39a9465d334a64
huyuan <422018883@qq.com>
2017-07-13 22:07:37 +0800

    dialog插件

--- src/js/list.js
+++ src/js/list.js
@@ -1,8 +1,32 @@
 $(function(){
     var delBtns = $('.table tr td:last-child button:last-child');
-    for(var i=0;i<delBtns.length;i++){
-        $(this).click(function(){
-            return i;
+    var editBtns = $('.table tr td:last-child button:first-child');
+    delBtns.bind('click',function(){
+        var d = dialog({
+            title: '删除',
+            content: '按钮回调函数返回 false 则不许关闭',
+            okValue: '确定',
+            ok: function () {
+                this.title('提交中…');
+                return false;
+            },
+            cancelValue: '取消',
+            cancel: function () {}
         });
-    }
+        d.show();
+    })
+    editBtns.bind('click',function(){
+        var d = dialog({
+            title: '编辑',
+            content: '按钮回调函数返回 false 则不许关闭',
+            okValue: '确定',
+            ok: function () {
+                this.title('提交中…');
+                return false;
+            },
+            cancelValue: '取消',
+            cancel: function () {}
+        });
+        d.show();
+    })
 })
