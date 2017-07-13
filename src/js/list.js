9c5789bad526869d3b91b6986fd0970cc679bec7
huyuan <422018883@qq.com>
2017-07-13 20:49:50 +0800

    pop

--- src/js/list.js
+++ src/js/list.js
@@ -1,3 +1,8 @@
 $(function(){
     var delBtns = $('.table tr td:last-child button:last-child');
+    for(var i=0;i<delBtns.length;i++){
+        $(this).click(function(){
+            return i;
+        });
+    }
 })
